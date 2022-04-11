7bd3507c760ac15ca92d315fc40dec8b883e20a5
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-11 14:36:46 +0100

    Added side bar navigation.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Box from "@mui/material/Box";
+import SideNav from "./components/SideNav/SideNav";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <BrowserRouter>
+      <CssBaseline />
+      <Box>
+        <SideNav />
+      </Box>
+    </BrowserRouter>
+  );
 };
 
 export default App;
