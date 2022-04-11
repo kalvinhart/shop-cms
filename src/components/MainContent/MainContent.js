32561d473facf14e412d4ebd553fa0459c1657c9
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-11 15:01:23 +0100

    Amended custom styles.

--- src/components/MainContent/MainContent.js
+++ src/components/MainContent/MainContent.js
@@ -2,7 +2,15 @@ import { Box } from "@mui/material";
 
 const MainContent = ({ children }) => {
   return (
-    <Box component="main" sx={{ display: "flex", flexDirection: "column" }}>
+    <Box
+      component="main"
+      sx={{
+        width: "100%",
+        display: "flex",
+        flexDirection: "column",
+        padding: "40px 40px",
+      }}
+    >
       {children}
     </Box>
   );
