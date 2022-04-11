8b2fe96196afbb324c841cc667c1ed33e5be49ad
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-11 14:56:03 +0100

    Added blank dashboard page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import CssBaseline from "@mui/material/CssBaseline";
 
 import Box from "@mui/material/Box";
+
 import SideNav from "./components/SideNav/SideNav";
 import MainContent from "./components/MainContent/MainContent";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 const App = () => {
   return (
@@ -16,7 +18,11 @@ const App = () => {
         }}
       >
         <SideNav />
-        <MainContent></MainContent>
+        <MainContent>
+          <Routes>
+            <Route path="/" element={<Dashboard />} />
+          </Routes>
+        </MainContent>
       </Box>
     </BrowserRouter>
   );
