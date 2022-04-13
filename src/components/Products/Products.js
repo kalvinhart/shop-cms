4c0243cc379a75528a598dcaf8edfde5b953191e
Kalvin <kalvinhart@hotmail.co.uk>
2022-04-13 18:09:26 +0100

    Added redux logic to fetch categories and show a loading state.

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,16 +1,42 @@
-import { Box, Paper, Typography } from "@mui/material";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+
+import { CircularProgress, Typography } from "@mui/material";
+import Form from "../shared/Form/Form";
 import ProductForm from "./ProductForm/ProductForm";
+import { loadCategories } from "../../slices/categorySlice";
 
 const Products = () => {
+  const dispatch = useDispatch();
+  const {
+    loading: categoryLoading,
+    categories,
+    error: categoryError,
+  } = useSelector((state) => state.category);
+
+  useEffect(() => {
+    if (!categoryLoading && !categories) {
+      dispatch(loadCategories());
+    }
+  }, [dispatch, loadCategories]);
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <Box>
-      <Paper sx={{ display: "flex", flexDirection: "column", padding: "40px" }}>
-        <Typography variant="h2" mb={4}>
-          Add a Product
-        </Typography>
-        <ProductForm />
-      </Paper>
-    </Box>
+    <>
+      {categoryLoading ? (
+        <CircularProgress />
+      ) : (
+        <>
+          <Typography variant="h2" mb={4}>
+            Add a Product
+          </Typography>
+          <Form component={ProductForm} submit={onSubmit} />
+        </>
+      )}
+    </>
   );
 };
 
