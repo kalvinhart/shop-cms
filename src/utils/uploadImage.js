bc82238acfa7b1bbed64fbcf96d2fe80290c5593
Kalvin <kalvinhart@hotmail.co.uk>
2022-05-24 14:39:09 +0100

    Created helper function for uploading images.

---
+++ src/utils/uploadImage.js
@@ -0,0 +1,24 @@
+import Axios from "../utils/axios";
+
+export const uploadImage = async (product, image) => {
+  if (typeof image !== "object") return product;
+
+  const imageInfo = {
+    id: product._id,
+    name: image[0].name,
+  };
+
+  const {
+    data: { url },
+  } = await Axios.post("/upload/s3url", imageInfo);
+
+  await Axios.put(url, image[0]);
+
+  const imageUrl = url.split("?")[0];
+
+  const { data: updatedProduct } = await Axios.patch(`/products/${product._id}`, {
+    imageUrl,
+  });
+
+  return updatedProduct;
+};
