e938f5aee1a4e3bcc9a4d10dd3f385e7ba518b58
Kalvin <kalvinhart@hotmail.co.uk>
2022-07-21 20:28:32 +0100

    migrated to TypeScript

---
+++ src/app/store.ts
@@ -0,0 +1,20 @@
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import categoryReducer from "./slices/categorySlice";
+import productReducer from "./slices/productSlice";
+import authReducer from "./slices/authSlice";
+
+import AuthService from "../services/AuthService";
+
+const authApi = AuthService;
+
+export const store = configureStore({
+  reducer: { product: productReducer, category: categoryReducer, auth: authReducer },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: { extraArgument: { authApi } },
+    }),
+});
+
+export type RootState = ReturnType<typeof store.getState>;
+
+export type AppDispatch = typeof store.dispatch;
