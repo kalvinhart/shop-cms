ec25a605f5167818d9efd8981abd4f40329f5b22
Kalvin <kalvinhart@hotmail.co.uk>
2022-10-06 13:34:29 +0100

    added react-query client to app

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,15 +1,22 @@
 import { Provider } from "react-redux";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { store } from "./store";
 
 import CssBaseline from "@mui/material/CssBaseline";
 import AppRoutes from "../routing/AppRoutes";
 
+const queryClient = new QueryClient();
+
 const App = () => {
   return (
     <Provider store={store}>
-      <CssBaseline />
-      <AppRoutes />
+      <QueryClientProvider client={queryClient}>
+        <CssBaseline />
+        <AppRoutes />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   );
 };
